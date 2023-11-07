8187e07e95bda0db41348b579d6b9b1475a663a0
Thameka <thalea.b@outlook.com>
2023-11-07 19:43:34 +0100

    style

--- game.js
+++ game.js
@@ -2,24 +2,37 @@ var startScreen
 var gameScreen
 var choiceBox
 var nextBox
+var nextButton
+var textSpeed
+var volume
+var imgcharacterSprite
+var imgCS
+var background
+var lbText 
+var lbCS
 
+
+
+  
+//#functions 
 window.onload = () => {
     startScreen = document.getElementById("startScreen");
     gameScreen = document.getElementById("gameScreen");
     choiceBox = document.getElementById("choiceBox");
     nextBox = document.getElementById("nextBox");
+    startScreen.style.display = "block";
     gameScreen.style.display = "none";
     choiceBox.style.display = "none";
-    nextBox.style.display = "none";
+    nextBox.style.display = "block";
 }
 
 function startGame() {
     startScreen.style.display = "none";
     gameScreen.style.display = "block";
-    nextBox.style.display = "block";
 }
 
 function NBoff() {
     nextBox.style.display=  "none";
     choiceBox.style.display= "block";
 }
+
