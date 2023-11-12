a98899cecf463b08700d3284b59f5713ad23efdf
Thameka <thalea.b@outlook.com>
2023-11-12 16:40:50 +0100

    dev

--- game.js
+++ game.js
@@ -25,11 +25,29 @@ class dialog {
 }
 
 class Background {
-    constructor (img) {
+    constructor(img) {
         this.img = img
     }
 }
 
+class Scene {
+    constructor(conversation) {
+        this.conversation = conversation;
+    }
+}
+
+class Conv {
+    constructor(events) {
+        this.events = events;
+    }
+}
+
+class ChangeScene {
+    constructor(scene) {
+        this.scene = scene;
+    }
+
+}
 //#functions 
 window.onload = () => {
     startScreen = document.getElementById("startScreen");
@@ -39,6 +57,7 @@ window.onload = () => {
     lbtext = document.getElementById("lbtext");
     lbname = document.getElementById("lbname");
     imgSprite = document.getElementById("imgSprite");
+    imgBackground = document.getElementById("imgBackground");
 
     startScreen.style.display = "block";
     gameScreen.style.display = "none";
@@ -60,31 +79,74 @@ function NBoff() {
 var LittleSnake = new character("Little Snake", getSpriteURL("Littlesnake"));
 var Ricky = new character("Ricky", getSpriteURL("Rickythefrog"));
 
-var Scene = [
-    new dialog(LittleSnake, "What a wonderful day! My house is warm and the flowers are always ssso lovely. The magical foresst is sssstrong today. A quiet sssstrength exudes from Her trees."),
-    new dialog(LittleSnake, "But I have a sssmall problem I have to ssssolve! Indeed, my ssscales are losing sssoftnesss, I wonder where I could find some ointment for them..."),
-    new dialog(LittleSnake, "Let'sss just take a sssmall walk in the foressst. I am sure the sssolution will come by itssself."),
-    new dialog(Ricky, "Oh hi Little Snake, what are you doing here?"),
-    new dialog(LittleSnake, "Ricky! What a good sssurprise! I was going to the foresst for a little walk. Do you want to come with me?"),
-    new dialog(Ricky, "Of course ! You know I love walks in the forest. I was myself heading to the pond!"),
-    new dialog (LittleSnake, "The pond! What a grandiose idea. Let'ss go to the pond!"),
-    new dialog (LittleSnake, "<i>(Maybe I will find some ointment there...)(</i>")
-];
+var Scene1 = new Scene([
+
+    new Conv([
+        new Background(getBackgroundURL("foresthouse")),
+        new dialog(LittleSnake, "What a wonderful day! My house is warm and the flowers are always ssso lovely. The magical foresst is sssstrong today. A quiet sssstrength exudes from Her trees."),
+        new dialog(LittleSnake, "But I have a sssmall problem I have to ssssolve! Indeed, my ssscales are losing sssoftnesss, I wonder where I could find some ointment for them..."),
+        new dialog(LittleSnake, "Let'sss just take a sssmall walk in the foressst. I am sure the sssolution will come by itssself."),
+        new dialog(Ricky, "Oh hi Little Snake, what are you doing here?"),
+        new dialog(LittleSnake, "Ricky! What a good sssurprise! I was going to the foresst for a little walk. Do you want to come with me?"),
+        new dialog(Ricky, "Of course ! You know I love walks in the forest. I was myself heading to the pond!"),
+        new dialog(LittleSnake, "The pond! What a grandiose idea. Let'ss go to the pond!"),
+        new dialog(LittleSnake, "<i>(Maybe I will find some ointment there...)</i>"),
+        new Background(getBackgroundURL("redforest")),
+        new dialog(LittleSnake, "Wow! The foresst is very pretty! What happened?"),
+    ]),
+
+    new Conv([
+        new Background(getBackgroundURL("forestpond")),
+        new dialog(Ricky, "There we go! My favourite pond."),
+        new dialog(LittleSnake, "Ricky! I didn't know thissss pond even exissssted!"),
+        new ChangeScene(Scene2),
+    ]),
+]);
+
+var Scene2 = new Scene([
+
+    new Conv([
+        new Background(getBackgroundURL("redforest")),
+        new dialog(Ricky, "Oops! we went back on that path and exited the pond."),
+    ]),
+]);
 
 var currentdialog = 0;
+var currentScene = Scene1;
+var currentConv = 0;
+
 
 function Next() {
-    var dialog = Scene[currentdialog];
-    lbtext.innerHTML = dialog.text;
-    lbname.innerHTML = dialog.character.name;
+    var event = currentScene[currentConv[currentdialog]];
+    var conversation = currentScene[currentConv];
     currentdialog++;
-    showSprite(dialog);
+
+    if (currentdialog == conversation.length) {
+        currentConv++;
+        Next()
+    }
+
+    if (event instanceof dialog) {
+        lbtext.innerHTML = event.text;
+        lbname.innerHTML = event.character.name;
+        showSprite(event);
+    }
+
+    if (event instanceof Background) {
+        showBackground(event);
+        Next();
+    }
+
+    if (event instanceof ChangeScene) {
+        changeScene(event.scene);
+    }
+
 }
 
-function showSprite(dialog) {
-    img = dialog.character.sprite;
+function showSprite(event) {
+    debugger;
+    img = event.character.sprite;
     imgSprite.src = img;
-    debugger
 }
 
 function getSpriteURL(path) {
@@ -95,7 +157,19 @@ function getBackgroundURL(path) {
     return (new URL("backgrounds/" + path + ".png", document.location)).href;
 }
 
-function showBackground(img) {
-    img = getBackgroundURL(img);
+function showBackground(background) {
+    var img = background.img;
     imgBackground.src = img;
+}
+
+function changeScene(scene) {
+    currentScene = scene;
+    currentConv = 0;
+    currentdialog = 0;
+    Next();
+}
+
+function changeConv(conv) {
+    currentConv = conv;
+    currentdialog = 0;
 }
